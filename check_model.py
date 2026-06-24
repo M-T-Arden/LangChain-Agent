4774adc86588b4fea3cfd71ea883ec20c8540e39
M-T-Arden <mttang2024@outlook.com>
2026-06-24 21:09:14 +0800

    [core] check if model support calls and adjust model

---
+++ check_model.py
@@ -0,0 +1,19 @@
+""" check if model supports tool calls """
+from langchain_ollama import ChatOllama
+from langchain.tools import tool
+
+model = ChatOllama(model="llama3.1:8b", base_url="http://localhost:11434")
+
+@tool
+def get_weather(city: str) -> str:
+    """获取指定城市的天气。"""
+    return f"{city}总是阳光明媚！"
+
+# 关键：显式 bind_tools
+model_with_tools = model.bind_tools([get_weather])
+
+response = model_with_tools.invoke("What's the weather in Tokyo?")
+
+print("type:", type(response))
+print("content:", response.content)
+print("tool_calls:", response.tool_calls)  # ← 这里必须非空才说明 tool call 真正触发了
--- first.py
+++ first.py
@@ -3,13 +3,14 @@ from langchain_ollama import ChatOllama
 from langchain.tools import tool
 
 model = ChatOllama(
-    model="qwen2.5-coder:7b",
+    model="llama3.1:8b", # qwen 2.5-coder:7b don't support tool calls
     base_url="http://localhost:11434", # 显式指向映射出来的本地端口
 )
 
 @tool
 def get_weather(city: str) -> str:
     """获取指定城市的天气。"""
+    print(f"获取 {city} 的天气信息...")
     return f"{city}总是阳光明媚！"
 
 tools=[get_weather]
@@ -27,8 +28,9 @@ for chunk in result:
     for node_name, node_output in chunk.items():
         if "messages" in node_output:
             # get node output and print the latest message content
-            latest_msg = node_output["messages"][-1]
-            print(f"\n[{node_name} 节点输出]: {latest_msg.content}")
+            latest_msg = node_output["messages"]
+            for msg in latest_msg:
+                print(f"\n[{node_name} 节点输出]: {msg.content}")
 
 result = agent.invoke(
     {"messages": [{"role": "user", "content": "What's the weather in New York?"}]}
